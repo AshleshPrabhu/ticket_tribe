c0132e844fefa1ddbdc1a5dca218f7729d70fa98
Ashlesh Prabhu <ashlesh.prabhu5@gmail.com>
2025-11-24 19:38:06 +0530

    try fixing enoent error

--- src/app/(dashboard)/page.tsx
+++ src/app/(dashboard)/page.tsx
@@ -1,4 +1,9 @@
-import { HomeView } from "@/modules/home/ui/views/home-view";
+import dynamic from 'next/dynamic';
+
+const HomeView = dynamic(() => import("@/modules/home/ui/views/home-view").then(mod => ({ default: mod.HomeView })), {
+  ssr: false,
+  loading: () => <div className="flex items-center justify-center min-h-screen">Loading...</div>
+});
 
 export default function Page() {
   return <HomeView />;
